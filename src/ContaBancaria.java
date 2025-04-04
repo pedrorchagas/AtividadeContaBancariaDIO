d10eaefe679a9c4d810c1789d1bba79548657407
Pedro <pedro.chagas@aluno.ueg.br>
2025-04-04 10:32:30 -0300

    Conclusão da clase "ContaBancaria" e implementação da lógica de interação com o usuário.

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -2,21 +2,25 @@ import exceptions.SaldoInsulficiente;
 
 public class ContaBancaria {
 
+    private final String nome;
     private double saldo;
-    private double chequeEspecial;
+    private double valorChequeEspecial;
+    private boolean chequeEspecial;
 
     // construtor da classe
-    public ContaBancaria(double saldoInicial) {
+    public ContaBancaria(double saldoInicial, String nome) {
         this.saldo = saldoInicial;
+        this.nome = nome;
 
         if (saldoInicial <= 500.00) {
             // se for menor ou igual a 500 o cheque especial será apenas R$50,00
-            this.chequeEspecial = 50.00;
+            this.valorChequeEspecial = 50.00;
         } else {
             // se for maior que 500 o cheque será 50% do valor inicial;
-            this.chequeEspecial = saldoInicial/2;
+            this.valorChequeEspecial = saldoInicial/2;
         }
     }
+
     // consultar saldo
     public double getSaldo(){
         return this.saldo;
@@ -36,7 +40,38 @@ public class ContaBancaria {
         }
     }
     // pagar um boleto
+    public void pagarBoleto(double valorBoleto) throws SaldoInsulficiente {
+        if (valorBoleto <= this.saldo){
+            this.saldo = this.saldo - valorBoleto;
+        } if (valorBoleto <= this.saldo + this.valorChequeEspecial) {
+            // Isso irá acontecer caso o valor do boleto seja maior que o saldo
+            valorBoleto = valorBoleto - this.saldo;
+            this.saldo = 0;
+            this.valorChequeEspecial = this.valorChequeEspecial - valorBoleto;
+            this.chequeEspecial = true;
+        }
+        else {
+            throw new SaldoInsulficiente("Saldo insulficiente para pagar o boleto");
+        }
+    }
 
     // metodo consutar cheque especial
+    public double consultarChequeEspecial() {
+        return this.saldo + this.valorChequeEspecial;
+    }
+
     // verificar se a conta esta usando cheque especial
+    public boolean usaChequeEspecial() {
+        return this.chequeEspecial;
+    }
+
+    // Puxa o nome do cliente
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValorChequeEspecial() {
+        return valorChequeEspecial;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Sistema bancário - Pedro Banks - Atividade DIO");
+
+        System.out.println("Criar cliente");
+        System.out.print("Nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.print("Valor inicial: ");
+        double saldoInicial = scanner.nextDouble();
+
+        ContaBancaria cliente = new ContaBancaria(saldoInicial, nomeCliente);
+        System.out.println("Cliente criado nome: " + cliente.getNome() + " Saldo: " + cliente.getSaldo() + " cheque especial: " + cliente.getValorChequeEspecial());
+
+        System.out.println("Escolha uma dessas opções a baixo: \n1 - Depositar dinheiro\n2 - Sacar dinheiro\n3 - Pagar um boleto\n");
+        scanner.nextLine();
+        String opcao = scanner.nextLine();
+        if (Objects.equals(opcao, "1")){
+           System.out.print("escolheu 1");
+
+        } else if (Objects.equals(opcao, "2")) {
+            System.out.print("escolheu 2");
+
+        } else if (Objects.equals(opcao, "3")) {
+            System.out.print("escolheu 3");
+
+        } else {
+            System.out.println("Escreva um número válido.");
+        }
 
     }
 }
